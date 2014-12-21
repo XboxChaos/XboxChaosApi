5bbdee1b3be633e08857253f8e2c8e775a9707fe
Alex Forbes-Reed (0xdeafcafe) <alexforbesreed@outlook.com>
2014-12-21 06:34:57 +0000

    tweaks

---
+++ README.md
@@ -0,0 +1,4 @@
+xboxchaosapi
+===
+
+API used by XboxChaos applications for updating and the such. Probally of no use to you. #yolo
---
+++ XboxChaosApi/Controllers/AboutController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace XboxChaosApi.Controllers
+{
+	public class AboutController : ApiController
+	{
+		// GET: api/1/About
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET: api/1/About/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST: api/1/About
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT: api/1/About/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE: api/1/About/5
+		public void Delete(int id)
+		{
+		}
+	}
+}
