69d093aa1ba8754326c8c1fcab770f3e70b7bf75
Alex Forbes-Reed (0xdeafcafe) <alexforbesreed@outlook.com>
2014-12-24 00:51:40 +0000

    fuck ignore

---
+++ XboxChaosApi/Models/Sql/ApplicationBranch.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XboxChaosApi.Models.Sql
+{
+	public class ApplicationBranch
+		: Audit
+	{
+		/// <summary>
+		/// Gets or Sets the id of the application branch.
+		/// </summary>
+		[Key]
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Gets or Sets the name of the application branch.
+		/// </summary>
+		[Required]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or Sets the git ref of the application branch.
+		/// </summary>
+		[Required]
+		public string Ref { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[Required]
+		public string RepoTree { get; set; }
+
+		/// <summary>
+		/// Gets or Sets the application that owns this branch.
+		/// </summary>
+		public virtual Application Application { get; set; }
+	}
+}
